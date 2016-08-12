c27c098c9c118ee091222b2f4e2c2703e7f4396b
_GaoFei <gdhgaofei@163.com>
2016-08-12 09:34:28 +0800

    Initial Commit

---
+++ 弹幕的实现/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  弹幕的实现
+//
+//  Created by 高得华 on 16/8/12.
+//  Copyright © 2016年 GaoFei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 弹幕的实现/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  弹幕的实现
+//
+//  Created by 高得华 on 16/8/12.
+//  Copyright © 2016年 GaoFei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
